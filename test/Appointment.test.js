ee99bacbe609b174075af8ecb8a8d0153041c3f6
Camilo Meassi Macoris <camilomacoris@hotmail.com>
2024-04-01 18:26:09 -0300

    Create first teste with Appointment Component

---
+++ test/Appointment.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { act } from "react-dom/test-utils";
+import { Appointment } from "../src/Appointment";
+
+describe("Appointment", () => {
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Rodolfo" };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+
+    document.body.appendChild(container);
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+
+    expect(document.body.textContent).toContain("Rodolfo");
+  });
+});
