e2ebd6a9353ef976ec7037dfb2e43c704901ad2c
Camilo Meassi Macoris <camilomacoris@hotmail.com>
2024-04-22 21:28:14 -0300

    create AppointmentsList test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
   let container;
@@ -27,5 +27,24 @@ describe("Appointment", () => {
 
     render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Jaqueline");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+  berforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => {
+      React.createRoot(container).render(component);
+    });
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
   });
 });
